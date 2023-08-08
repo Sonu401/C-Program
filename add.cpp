a58813179d8adc4f7a74b32f27b44648d1455d94
SONU KUMAR KUSHWAHA <113274705+Sonu401@users.noreply.github.com>
2023-08-08 13:47:20 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,add;
+	printf("Enter two number:");
+	scanf("%d%d",&a,&b);
+	add=a+b;
+	printf("addition of two numbers: %d" ,add);
+	return 0;
+}
---
+++ sum_bet_two_no.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int n1,n2,s=0;
+	printf("Enter range:");
+	scanf("%d%d",&n1,&n2);
+	while(n1<=n2)
+	{
+		s=s+n1;
+		n1=n1+1;
+	}
+	printf("sum %d",s);
+	return 0;
+}
