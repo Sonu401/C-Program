931b11778e5a8417d3c6d10e3a258fa6bb8fe358
SONU KUMAR KUSHWAHA <113274705+Sonu401@users.noreply.github.com>
2023-08-09 17:44:48 +0530

    Add files via upload

---
+++ ASCII_Value.cpp
@@ -0,0 +1,15 @@
+//ASCII value program
+//A---->65
+//a---->97
+//space----> 32
+//0------->48
+#include<stdio.h>
+int main()
+{
+	char ch;
+	printf("Enter any character:");
+	scanf("%c",&ch);
+	
+	printf("%d",ch);
+	return 0;
+}
